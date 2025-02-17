15cb1b3219596d13de8f259b7446e5e8e56c12c7
Dhanush Gowda mv <dhanushvenu199@gmail.com>
2025-02-17 21:20:31 +0530

    compny setup

--- frontend/src/components/admin/CompanySetup.jsx
+++ frontend/src/components/admin/CompanySetup.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import Navbar from '../shared/Navbar'
+import { Button } from '../ui/button'
+import { ArrowLeft } from 'lucide-react'
 
 const CompanySetup = () => {
   return (
-    <div>CompanyS</div>
+    <div><Navbar/>
+    <div className="max-w-xl mx-auto my-10">
+      <form action="">
+        <Button>
+          <ArrowLeft/>
+          <span>Back</span>
+        </Button>
+      </form>
+    </div>
+    </div>
   )
 }
 
